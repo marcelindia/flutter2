6e2d5f750528e7364a957a579e49e1388c5ef612
Diana Marcelin <marcelindiana@yahoo.com>
2024-06-02 18:49:55 -0400

    code challenge

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,16 @@ void main() {
   runApp(
      MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.lightBlue,
+        backgroundColor: Colors.white,
         appBar: AppBar(
-          title: const Text('I am Rich'),
+          title: const Text('Bob Burgers'),
           foregroundColor: Colors.white,
           backgroundColor: Colors.blueGrey[900],
+          centerTitle: true,
         ),
           body: const Center (
             child: Image(
-            image: NetworkImage('https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-980x653.jpg'),
+              image: NetworkImage('https://threedio-cdn.icons8.com/VjjrFnzBUALrfRGBDLcinVewN88Glb49FX6mEg8-p9w/rs:fit:1024:1024/czM6Ly90aHJlZWRp/by1wcm9kL3ByZXZp/ZXdzLzMxNy8wMmMy/ZjIzMS00NzM0LTQz/NTAtYjVjYS1lZjM0/M2MyYzc4MzMucG5n.png'),
           ),
           ),
       ),
